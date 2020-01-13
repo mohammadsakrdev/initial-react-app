9b937be20d6f5fda93990d47933c1b07c22330c1
Mohammad Sakr <mohamed.sakr@bosta.co>
2020-01-14 00:02:20 +0200

    Done

--- src/components/routes/Login.js
+++ src/components/routes/Login.js
@@ -1,9 +1,15 @@
 import React, { Component } from 'react';
 import { EnhancedLoginForm } from 'components/forms/LoginForm/EnhancedLoginForm';
+import { Title } from 'components/text/Title';
 
 class Login extends Component {
   render() {
-    return <EnhancedLoginForm />;
+    return (
+      <div>
+        <Title> Login </Title>
+        <EnhancedLoginForm />
+      </div>
+    );
   }
 }
 
--- src/components/routes/Signup.js
+++ src/components/routes/Signup.js
@@ -1,9 +1,15 @@
 import React, { Component } from 'react';
 import { EnhancedSignupForm } from 'components/forms/signupForm/EnhancedSignupform';
+import { Title } from 'components/text/Title';
 
 class Signup extends Component {
   render() {
-    return <EnhancedSignupForm />;
+    return (
+      <div>
+        <Title> Signup </Title>
+        <EnhancedSignupForm />
+      </div>
+    );
   }
 }
 
