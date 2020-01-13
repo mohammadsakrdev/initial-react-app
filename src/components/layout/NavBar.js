6be017accee52938d2d2f5cc5f86fbc341ad0a34
Mohammad Sakr <mohamed.sakr@bosta.co>
2020-01-13 22:46:37 +0200

    Done with logout

--- src/components/layout/NavBar.js
+++ src/components/layout/NavBar.js
@@ -1,18 +1,32 @@
 import React, { Component } from 'react';
 import { NavLink } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { unpersistMyInfo } from '../services/persistence';
 
 const NavBarLink = ({ to, label }) => (
-  <li>
+  <li className='nav-item'>
     <NavLink to={to} activeClassName='active' className='nav-link'>
       {label}
     </NavLink>
   </li>
 );
 
-class Navbar extends Component {
+const mapDispatchToProps = dispatch => ({
+  loggedOut: () => dispatch({ type: 'LOGGED_OUT' })
+});
+
+const mapStateToProps = state => ({
+  isAuthenticated: state.isAuthenticated
+});
+
+class PNavbar extends Component {
+  logout = () => {
+    this.props.loggedOut();
+    unpersistMyInfo();
+  };
   render() {
     return (
-      <nav className='navbar navbar-expand-lg navbar-light bg-light'>
+      <nav className='navbar navbar-expand-lg navbar-light bg-light mb-4'>
         <a className='navbar-brand'>Bikes Rental</a>
         <button
           className='navbar-toggler'
@@ -28,16 +42,30 @@ class Navbar extends Component {
 
         <div className='collapse navbar-collapse' id='navbarSupportedContent'>
           <ul className='navbar-nav mr-auto'>
-            <NavBarLink to='/login' label='Login'></NavBarLink>
-            <NavBarLink to='/signup' label='Signup'></NavBarLink>
-            <NavBarLink to='/profile' label='My Profile'></NavBarLink>
-            <NavBarLink to='/users' label='Users'></NavBarLink>
-            <NavBarLink to='/bikes' label='Bikes'></NavBarLink>
+            <NavBarLink to='/login' label='Login' />
+            <NavBarLink to='/signup' label='Signup' />
+            {this.props.isAuthenticated && (
+              <NavBarLink to='/profile' label='My Profile' />
+            )}
           </ul>
+          {this.props.isAuthenticated && (
+            <ul className='navbar-nav ml-auto mr-5'>
+              <li className='nav-item' onClick={this.logout}>
+                <NavLink
+                  to='login'
+                  activeClassName='active'
+                  className='nav-link'
+                >
+                  Logout
+                </NavLink>
+              </li>
+            </ul>
+          )}
         </div>
       </nav>
     );
   }
 }
 
+const Navbar = connect(mapStateToProps, mapDispatchToProps)(PNavbar);
 export default Navbar;
